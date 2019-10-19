fcf3159895c2a68419c89f71afbe5510c48a8267
sumeet1030 <43828103+sumeet1030@users.noreply.github.com>
2019-10-19 17:23:58 +0530

    Update movie_recommender.py

--- movie_recommender.py
+++ movie_recommender.py
@@ -2,29 +2,49 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-###### helper functions. Use them when needed #######
 def get_title_from_index(index):
-	return df[df.index == index]["title"].values[0]
+ return df[df.index == index]["title"].values[0]
 
 def get_index_from_title(title):
-	return df[df.title == title]["index"].values[0]
-##################################################
-
-##Step 1: Read CSV File
-
-##Step 2: Select Features
-
-##Step 3: Create a column in DF which combines all selected features
-
-##Step 4: Create count matrix from this new combined column
-
-##Step 5: Compute the Cosine Similarity based on the count_matrix
-
-movie_user_likes = "Avatar"
-
-## Step 6: Get index of this movie from its title
-
-## Step 7: Get a list of similar movies in descending order of similarity score
+ return df[df.title == title]["index"].values[0]
 
 
-## Step 8: Print titles of first 50 movies
+df=pd.read_csv("movie_dataset.csv") #loading the dataset
+
+
+feature=["keywords","cast","crew","director"]
+
+
+
+for i in feature:
+    df[i]=df[i].fillna('')
+
+
+df["features"]=df["keywords"]+df["cast"]+df["crew"]+df["director"]
+
+
+
+cv=CountVectorizer()
+
+count_matrix=cv.fit_transform(df["features"])
+
+
+
+similarity_score=cosine_similarity(count_matrix)
+
+
+movie_user_likes="Spectre"
+
+movie_index=get_index_from_title(movie_user_likes)
+
+similar_movies=list(enumerate(similarity_score[movie_index]))
+
+sorted_similar_movies=sorted(similar_movies,key=lambda x:x[1],reverse=True)
+
+i=0
+for movie in sorted_similar_movies:
+    if i<10:
+        print(get_title_from_index(movie[0]))
+        i=i+1
+    else:
+        break
